87839f6d11f4323906e6f6ff96d2872e0c76c8f8
IcaroMachadoCarvalho <icarocarvalho028@gmail.com>
2024-04-27 16:26:29 -0300

    Adiciona interação com seção de compras e mensagem de aviso

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let indexSlide = 0;
 
 function currentSlide(index){
     clearSlide();
-    console.log(index);
+    // console.log(index);
     slides[index].style.display = "block";
 }
 
@@ -37,6 +37,8 @@ btnNext.addEventListener("click", ()=>{
 clearSlide();
 currentSlide(0);
 
+// Navbar
+
 const navBar = document.querySelector("#navBar");
 const menuBtn = document.querySelector("#menuNav");
 const closeNav = document.querySelector("#closeNav"); 
@@ -47,11 +49,59 @@ menuBtn.addEventListener("click", ()=>{
     document.querySelector("#backgroundNav").style.opacity = 0.5;
     document.querySelector("#backgroundNav").style.zIndex = 7;
 });
-
+//colocar clicar fora fecha tbm
 closeNav.addEventListener("click", ()=>{
     navBar.style.width = "0%";
     navBar.style.opacity = 0;
     document.querySelector("#backgroundNav").style.opacity = 0;
     document.querySelector("#backgroundNav").style.zIndex = -10;
 });
+
+
+// Shopping section
+const btnMinus = document.querySelector("#btnMinus");
+const btnPlus = document.querySelector("#btnPlus");
+let counter = document.querySelector("#counter");
+let quantity = 0;
+
+counter.innerHTML = quantity;
+
+function updatePrices(qtd){
+    document.querySelector("#currentPrice").innerHTML = qtd * 125 + ".00";
+    document.querySelector("#originalPrice").innerHTML = qtd * 250 + ".00";
+}
+
+btnMinus.addEventListener("click", ()=>{
+    if(quantity <= 1 || quantity == 0){
+        console.log(quantity);
+        quantity = 0;   
+        counter.innerHTML = quantity;
+        updatePrices(1);
+    }else{
+        counter.innerHTML = --quantity;
+        // console.log(quantity);
+        updatePrices(quantity);
+    }
+});
+btnPlus.addEventListener("click", ()=>{
+    counter.innerHTML = ++quantity;
+    // console.log(quantity);
+    updatePrices(quantity);
+});
+
+addCart.addEventListener("click", ()=>{
+    let message = document.querySelector("#controllerMassage");
+    if(quantity > 0){
+        quantity = 0;   
+        counter.innerHTML = quantity;
+        updatePrices(1);
+        message.style.display = "none";
+    }else{
+        message.style.display = "block";
+        window.scrollTo(0, document.body.scrollHeight);
+        // alert("Adicione no mínimo um produto");
+    }
+});
+
+
 
